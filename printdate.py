f440b5a2802ce1493700e6cb473a6b6b8ef17b8e
Vernon Swanepoel <vernon@sandcurves.com>
2016-04-24 07:59:10 +0000

    I've got the main routine working

--- printdate.py
+++ printdate.py
@@ -14,19 +14,41 @@ def getdate():
     return datetime.datetime(year, month, day)
 
 def determine_format():
-    if len(sys.argv) < 1:
+    if len(sys.argv) > 1:
         format = sys.argv[1]
         if format not in ["euro", "usa"]:
             print("Unrecognized format, select from 'euro' or 'usa'")
-            return False
+            format = False
     else:
         format = "euro"
+    return format
 
+class print_dates:
+    def __init__(self, format, start_date, end_date):
+        self.format = format
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def printit(self, date):
+        if self.format == "euro":
+            print("{0:%A, %d %B %Y}\n".format(date))
+        else:
+            print("{0:%A, %B %d %Y}\n".format(date))
+
+    def p_routine(self):
+        date = self.start_date
+        while date <= self.end_date:
+            self.printit(date)
+            date += datetime.timedelta(days=1)
 
 if __name__ == "__main__":
     format = determine_format()
     if format:
+        print("Start date")
         start_date = getdate()
-        print("{0:%F}".format(start_date))
+        print("End date")
+        end_date = getdate()
+        pdate = print_dates(format, start_date, end_date)
+        pdate.p_routine()
 
 
